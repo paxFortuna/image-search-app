e29da1f7cf43e40355d119042928267aa65b3ecf
fortuna <avidia38@gmail.com>
2022-07-23 20:12:01 +0900

    modified: url => add 'query' of getImage() method's parmeter in 'q'

--- lib/screens/image_search_screen.dart
+++ lib/screens/image_search_screen.dart
@@ -23,9 +23,10 @@ class _ImageSearchAppState extends State<ImageSearchApp> {
   }
 
   Future<List<Photo>> getImage(String query) async {
+    // url search-query refactoring
     Uri url = Uri.parse(
-        'https://pixabay.com/api'
-            '/?key=26655862-d25160d651ed15b14be08cf35&q=apple&image_type=photo');
+        'https://pixabay.com/api/?key=26655862-d25160d651ed15b14be08cf35&q'
+            '=$query&image_type=photo');
     http.Response response = await http.get(url);
     print('Response status: ${response.statusCode}');
 
